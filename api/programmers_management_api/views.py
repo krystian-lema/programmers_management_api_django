c42d15f864e470cfe3a2aa02be9242bd4c7b3831
Krystian Lema <krystian.lema@gmail.com>
2019-03-10 14:05:24 +0100

    Add basic views.

--- api/programmers_management_api/views.py
+++ api/programmers_management_api/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Team, Programmer, Language, Paradigm
+from .serializers import  TeamSerializer, ProgrammerSerializer, LanguageSerializer, ParadigmSerializer
 
-# Create your views here.
+class ListTeamView(viewsets.ModelViewSet):
+    queryset = Team.objects.all()
+    serializer_class = TeamSerializer
+
+class ListProgrammerView(viewsets.ModelViewSet):
+    queryset = Programmer.objects.all()
+    serializer_class = ProgrammerSerializer
+
+class ListLanguageView(viewsets.ModelViewSet):
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
+
+class ListParadigmView(viewsets.ModelViewSet):
+    queryset = Paradigm.objects.all()
+    serializer_class = ParadigmSerializer
