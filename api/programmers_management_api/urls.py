db17e402146bcfc090943db08ac06f0e698e64db
Krystian Lema <krystian.lema@gmail.com>
2019-03-10 15:55:22 +0100

    Add endpoints for creating relations: programmer-language and language-paradigm.

--- api/programmers_management_api/urls.py
+++ api/programmers_management_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+from django.conf.urls import url
 
 router = routers.DefaultRouter()
 
@@ -10,5 +11,7 @@ router.register('languages', views.ListLanguageView)
 router.register('paradigms', views.ListParadigmView)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    url(r'^programmers/(?P<programmer_id>[0-9]+)/languages/rel/(?P<language_id>[0-9]+)', views.programmer_language_rel),
+    url(r'^languages/(?P<language_id>[0-9]+)/paradigms/rel/(?P<paradigm_id>[0-9]+)', views.language_paradigm_rel),
 ]
--- api/programmers_management_api/views.py
+++ api/programmers_management_api/views.py
@@ -22,3 +22,17 @@ class ListLanguageView(viewsets.ModelViewSet):
 class ListParadigmView(viewsets.ModelViewSet):
     queryset = Paradigm.objects.all()
     serializer_class = ParadigmSerializer
+
+@api_view(['PUT'])
+def programmer_language_rel(request, programmer_id, language_id):
+    programmer = Programmer.objects.filter(pk=programmer_id).first()
+    language = Language.objects.filter(pk=language_id).first()
+    programmer.languages.add(language)
+    return Response({"result": "Language has been added."})
+
+@api_view(['PUT'])
+def language_paradigm_rel(request, language_id, paradigm_id):
+    language = Language.objects.filter(pk=language_id).first()
+    paradigm = Paradigm.objects.filter(pk=paradigm_id).first()
+    language.paradigms.add(paradigm)
+    return Response({"result": "Paradigm has been added."})
