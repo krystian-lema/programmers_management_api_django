631a8f403e5c68a24718c70c41d97c16647d7cf6
Krystian Lema <krystian.lema@gmail.com>
2019-03-10 13:54:49 +0100

    Define models with relations.

--- api/programmers_management_api/models.py
+++ api/programmers_management_api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=40, null=False)
+
+class Programmer(models.Model):
+    firstname = models.CharField(max_length=40, null=False)
+    lastname = models.CharField(max_length=40, null=False)
+    team = models.ForeignKey(Author, on_delete=models.CASCADE)
+    languages = models.ManyToManyField(Language)
+
+class Language(models.Model):
+    name = models.CharField(max_length=40, null=False)
+    programmers = models.ManyToManyField(Programmer)
+    paradigms = models.ManyToManyField(Paradigm)
+
+class Paradigm(models.Model):
+    name = models.CharField(max_length=20, null=False)
+    languages = models.ManyToManyField(Language)
